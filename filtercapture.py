ccfa5ae06ea473c2c97ff7b377f22a092c8c5195
Melissa Harris <53122980+Melissa-Harris-umich@users.noreply.github.com>
2021-04-14 00:20:17 -0400

    updated filtercapture to create video

--- filtercapture.py
+++ filtercapture.py
@@ -5,16 +5,14 @@ import numpy as np
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
 
-halo = cv2.imread('vendetta.png')
+filter = cv2.imread('mask.png')
 
   # creating 3 variables for the 3 attributes to be assigned to
-orig_halo_h,orig_halo_w,halo_channels = halo.shape
-# Haar Cascades and many facial recognition algorithms require images to be in grayscale
-#gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray_halo = cv2.cvtColor(halo, cv2.COLOR_BGR2GRAY)
+orig_filter_h,orig_filter_w,filter_channels = filter.shape
+gray_filter = cv2.cvtColor(filter, cv2.COLOR_BGR2GRAY)
 
 
-ret, original_mask = cv2.threshold(gray_halo, 10, 255, cv2.THRESH_BINARY_INV)
+ret, original_mask = cv2.threshold(gray_filter, 10, 255, cv2.THRESH_BINARY_INV)
 original_mask_inv = cv2.bitwise_not(original_mask)
 
 #read videoq
@@ -22,12 +20,17 @@ cap = cv2.VideoCapture(0)
 ret, img = cap.read()
 img_h, img_w = img.shape[:2]
 
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))  # 20 fps
 
 while True:   #continue to run until user breaks loop
     
     #read each frame of video and convert to gray
     ret, img = cap.read()
+
+
+    
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     #find faces in image using classifier
@@ -47,55 +50,55 @@ while True:   #continue to run until user breaks loop
         face_y1 = y
         face_y2 = face_y1 + face_h
 
-        #halo size in relation to face by scaling
-        halo_width = int(1.5 * face_w)
-        halo_height = int(halo_width * orig_halo_h / orig_halo_w)
-        #Test on halo
-        #halo_height = int(halo_width * .5)
+        #filter size in relation to face by scaling
+        filter_width = int(1.5 * face_w)
+        filter_height = int(filter_width * orig_filter_h / orig_filter_w)
+        #Test on filter
+        #filter_height = int(filter_width * .5)
         
-        #setting location of coordinates of halo
-        halo_x1 = face_x2 - int(face_w/2) - int(halo_width/2)
-        halo_x2 = halo_x1 + halo_width
-        halo_y1 = face_y1 - int(face_h*.33)
-        halo_y2 = halo_y1 + halo_height 
+        #setting location of coordinates of filter
+        filter_x1 = face_x2 - int(face_w/2) - int(filter_width/2)
+        filter_x2 = filter_x1 + filter_width
+        filter_y1 = face_y1 - int(face_h*.33)
+        filter_y2 = filter_y1 + filter_height 
 
         #check to see if out of frame
-        if halo_x1 < 0:
-            halo_x1 = 0
-        if halo_y1 < 0:
-            halo_y1 = 0
-        if halo_x2 > img_w:
-            halo_x2 = img_w
-        if halo_y2 > img_h:
-            halo_y2 = img_h
+        if filter_x1 < 0:
+            filter_x1 = 0
+        if filter_y1 < 0:
+            filter_y1 = 0
+        if filter_x2 > img_w:
+            filter_x2 = img_w
+        if filter_y2 > img_h:
+            filter_y2 = img_h
 
         #Account for any out of frame changes
-        halo_width = halo_x2 - halo_x1
-        halo_height = halo_y2 - halo_y1
+        filter_width = filter_x2 - filter_x1
+        filter_height = filter_y2 - filter_y1
 
-        #resize halo to fit on face
-        halo = cv2.resize(halo, (halo_width,halo_height), interpolation = cv2.INTER_AREA)
-        mask = cv2.resize(original_mask, (halo_width,halo_height), interpolation = cv2.INTER_AREA)
-        mask_inv = cv2.resize(original_mask_inv, (halo_width,halo_height), interpolation = cv2.INTER_AREA)
+        #resize filter to fit on face
+        filter = cv2.resize(filter, (filter_width,filter_height), interpolation = cv2.INTER_AREA)
+        mask = cv2.resize(original_mask, (filter_width,filter_height), interpolation = cv2.INTER_AREA)
+        mask_inv = cv2.resize(original_mask_inv, (filter_width,filter_height), interpolation = cv2.INTER_AREA)
 
-        #take ROI for halo from background that is equal to size of halo image
-        roi = img[halo_y1:halo_y2, halo_x1:halo_x2]
+        #take ROI for filter from background that is equal to size of filter image
+        roi = img[filter_y1:filter_y2, filter_x1:filter_x2]
 
-        #original image in background (bg) where halo is not present
+        #original image in background (bg) where filter is not present
         roi_bg = cv2.bitwise_and(roi,roi,mask = mask)
-        roi_fg = cv2.bitwise_and(halo,halo,mask=mask_inv)
+        roi_fg = cv2.bitwise_and(filter,filter,mask=mask_inv)
         dst = cv2.add(roi_bg,roi_fg)
 
         #put back in original image
-        img[halo_y1:halo_y2, halo_x1:halo_x2] = dst
+        img[filter_y1:filter_y2, filter_x1:filter_x2] = dst
 
         break
-
+    
     winname ="Video Capture"
     cv2.namedWindow(winname)        # Create a named window
     cv2.moveWindow(winname, 40,30)  # Move it to (40,30)
     cv2.imshow(winname,img) #display image
-
+    out.write(img)
     #if user pressed 'q' break
     if cv2.waitKey(1) == ord('q'): # 
         break;
